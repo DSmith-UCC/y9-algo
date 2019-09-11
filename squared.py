b02a565640cb6d8ff575ebf40ab8c233fc357e26
DSmith-UCC <55198102+DSmith-UCC@users.noreply.github.com>
2019-09-11 10:32:01 -0400

    Add files via upload

---
+++ squared.py
@@ -0,0 +1,15 @@
+'''
+squared.py - simple input-process-out example
+'''
+#mynum = input("enter a number (e.g., 7): ") # get user input as string
+#mynum_int = int(mynum) # cast string to integer for math expression
+#mynum_output = mynum_int * mynum_int # assign value of math expression to new variable
+#mynum_str = str(mynum_output) # cast integer to a string for output using print() function
+#print("Your answer is " + mynum_str) # output answer to screen using print()
+
+
+square = input("what number do you want to square: ")
+square_int = int(square)
+square_answer = square_int*square_int
+square_your_answer_is= str(square_answer)
+Square_finale= print("your number squared is "+square_your_answer_is)
